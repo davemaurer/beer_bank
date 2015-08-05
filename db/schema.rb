c72812d82c9fb987c05f3787069ca4a48152be5b
David Maurer <davemaurermail@gmail.com>
2015-08-05 16:42:41 -0600

    created models for beers and lists.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150805172523) do
+ActiveRecord::Schema.define(version: 20150805224103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "beers", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "rating"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
---
+++ spec/factories/beers.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :beer do
+    name "MyString"
+rating 1
+description "MyText"
+  end
+
+end
